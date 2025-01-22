a01e8dbe22683ccecb64133c92fad198913bc656
José Calderón - Duoc Account <tutuyosoy1111@gmail.com>
2025-01-21 23:19:09 -0300

    Avances del curso.

---
+++ JavaScript/Leccion25/03-OperadoresJs.js
@@ -0,0 +1,24 @@
+// * SUMA
+let a = 3, b = 2
+let z = a + b
+console.log("Resultado de la suma: "+z);
+
+// * RESTA
+z = a - b
+console.log("Resultado de la suma: "+z);
+
+// * MULTIPLICACIÓN
+z = a * b
+console.log("Resultado de la multiplicación: " + z);
+
+// * DIVISIÓN
+z = a / b
+console.log("Resultado de la división: " + z);
+
+// * RESIDUO
+z = a % b
+console.log("Resultado del residuo de la división: " + z);
+
+// * EXPONENTES
+z = a ** b
+console.log("Resultado del exponente: " + z);

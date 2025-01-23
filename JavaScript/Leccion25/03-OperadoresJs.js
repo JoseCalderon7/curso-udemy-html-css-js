8f95e4ca12ef9e4cd28169a9fafd861f08a96abf
José Calderón - Duoc Account <tutuyosoy1111@gmail.com>
2025-01-23 19:27:46 -0300

    Avances del curso.

--- JavaScript/Leccion25/03-OperadoresJs.js
+++ JavaScript/Leccion25/03-OperadoresJs.js
@@ -22,3 +22,43 @@ console.log("Resultado del residuo de la división: " + z);
 // * EXPONENTES
 z = a ** b
 console.log("Resultado del exponente: " + z);
+
+// * INCREMENTO
+// PreIncremento (el operador ++ antes de la variable)
+z = ++a
+console.log(a);
+console.log(z);
+
+// Post-Incremento (el operador ++ despues de la variable)
+z = b++
+console.log(b);
+console.log(z);
+
+// * DECREMENTO
+// PreDecremento
+z = --a
+console.log(a);
+console.log(z);
+
+// Post-Decremento
+z = b--;
+console.log(b);
+console.log(z);
+
+// * PROCEDENCIA DE EXPRECIONES
+let c = 1;
+let d = 4;
+z = a * b + c
+console.log(z);
+
+z = c + a * b
+console.log(z);
+
+z = a * b + c / d
+console.log(z);
+
+z = c + a * b / d
+console.log(z);
+
+z = (c + a) * b / c
+console.log(z);

827c8d95ee7716c901608839d94b364056570ed3
José Calderón - Duoc Account <tutuyosoy1111@gmail.com>
2025-01-24 18:33:57 -0300

    Avances del curso.

---
+++ JavaScript/Leccion27/05-CiclosJavaScript.js
@@ -0,0 +1,20 @@
+//* CICLO WHILE ~ más común.
+let contador = 0
+
+while (contador < 3) {
+    console.log("Número del contador: " + contador);
+    contador++
+}
+
+//* CICLO DO-WHILE ~ menos recurrente.
+do {
+    console.log("Número del contador: " + contador);
+    contador++
+} while (contador < 6);
+
+
+//* CICLO FOR 
+for(let contador2 = 0; contador2 < 3; contador2++ ){
+    console.log("Hola");
+}
+

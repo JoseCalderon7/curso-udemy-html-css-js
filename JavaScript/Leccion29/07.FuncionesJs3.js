2111644477a314457f7e22367cc95389f87267f9
José Calderón <tutuyosoy1111@gmail.com>
2025-01-26 20:12:21 -0300

    Avances del curso.

--- JavaScript/Leccion29/07.FuncionesJs3.js
+++ JavaScript/Leccion29/07.FuncionesJs3.js
@@ -10,3 +10,34 @@ function sumarTodo() {
 }
 
 console.log(resultado);
+
+//* PASOS POR VALOR EN "FUNCIONES"
+let x = 10 //VALOR PRIMITIVO
+
+function cambiarValor(a) {
+    a = 20
+}
+
+//* PASO POR VALOR
+cambiarValor(x)
+console.log(x);
+
+//! CUANDO SE TERMINA DE USAR EL MÉTODO LA VARIABLE SE DESTRUYE.
+// console.log(a);
+
+//* CREACIÓN DE UN OBJETO - PERSONA ALMACENA UNA "REFERENCIA"...
+const persona = {
+    nombre : "Juan",
+    apellido : "Perez",
+    edad : 27
+}
+
+function cambiarValorObjeto(p1) {
+    // SE PASA EL VALOR DE LA REFERENCIA
+    p1.nombre = "Rosa"
+    p1.apellido = "Gomez"
+}
+
+//* PASO POR REFERENCIA - ACCEDER A LOS VALORES Y MODIFICARLOS
+cambiarValorObjeto(persona)
+console.log(persona);

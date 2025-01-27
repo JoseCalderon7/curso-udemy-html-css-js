e5ea2ea193c9f3303770435282d9a5237c2fb6c7
José Calderón <tutuyosoy1111@gmail.com>
2025-01-27 19:30:55 -0300

    Avances del curso.

---
+++ JavaScript/Leccion30/08-Objects.js
@@ -0,0 +1,14 @@
+let x = 10
+console.log(x.lenght);
+
+let persona = {
+    nombre : "Juan",
+    apellido : "Perez",
+    edad : 48
+}
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.edad);
+
+console.log(persona);

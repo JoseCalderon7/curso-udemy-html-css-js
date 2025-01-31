f4dc5306c1ed5dfcfaaa2f148cefa3c22d7b79a1
José Calderón <tutuyosoy1111@gmail.com>
2025-01-31 19:54:58 -0300

    Avances del curso.

---
+++ JavaScript/Leccion36/14-StrictJs.js
@@ -0,0 +1,17 @@
+"use strict" //! MODO ESTRICTO | EXTERNO
+
+let y = 10;
+console.log(y);
+
+// x = 10;
+// console.log(x);
+
+miFuncion()
+
+function miFuncion(){
+    // "use strict"
+    //! MODO ESTRICTO | INTERNO
+    let z = 15;
+    console.log(z);
+    
+}
